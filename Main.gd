428002e216d958c6af21d59feae0cb1a1ef5f52b
cyberfencer <tboekbinder27@bfhsla.org>
2025-02-11 12:21:08 -0600

    finished

--- Main.gd
+++ Main.gd
@@ -9,25 +9,24 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
-		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 
 
 func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
+	# creates a new instance of the mob scene
 	var mob = mob_scene.instantiate()
 
-	# Choose a random location on the SpawnPath.
+	# choose a random location on the spawn path
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 
-	# Communicate the spawn location and the player's location to the mob.
+	# communicates the spawn location and the player's location to the mob
 	var player_position = $Player.position
 	mob.initialize(mob_spawn_location.position, player_position)
 
-	# Spawn the mob by adding it to the Main scene.
+# spawn mob
 	add_child(mob)
-	# We connect the mob to the score label to update the score upon squashing a mob.
+	
 	mob.squashed.connect($UserInterface/ScoreLabel._on_Mob_squashed)
 
 
--- Mob.gd
+++ Mob.gd
@@ -1,12 +1,9 @@
 extends CharacterBody3D
 
-# Emitted when the player jumped on the mob.
 signal squashed
 
-## Minimum speed of the mob in meters per second.
-@export var min_speed = 10
-## Maximum speed of the mob in meters per second.
-@export var max_speed = 18
+@export var min_speed = 5
+@export var max_speed = 25
 
 
 func _physics_process(_delta):
@@ -17,10 +14,9 @@ func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 
+# spice the game up with varied speed
 	var random_speed = randf_range(min_speed, max_speed)
-	# We calculate a forward velocity first, which represents the speed.
 	velocity = Vector3.FORWARD * random_speed
-	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 	$AnimationPlayer.speed_scale = random_speed / min_speed
